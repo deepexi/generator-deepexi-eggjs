cad1f9c96b9e1aa5485e6b225b69df39f8840696
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-01 15:32:48 +0800

    introduce eslint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,44 +1,44 @@
-var Generator = require('yeoman-generator');
+'use strict'
+var Generator = require('yeoman-generator')
 
-const _ = require('lodash');
-const path = require('path');
-const fileUtils = require('../util/file_utils');
+const _ = require('lodash')
+const path = require('path')
+const fileUtils = require('../util/file_utils')
 
 module.exports = class extends Generator {
-    catch(e) {
-        if (e) {
-            console.log(e)
-        }
-    };
-
-    async prompting() {
-        const answers = await this.prompt([
-            {
-                type: "input",
-                name: "projectName",
-                message: "Your project name",
-                default: 'deepexi-eggjs'
-            },
-            {
-                type: "input",
-                name: "projectTitle",
-                message: "Your project title",
-                default: 'DeepEXI EggJs'
-            },
-        ]);
-        this.props = answers;
+  catch (e) {
+    if (e) {
+      console.log(e)
     }
+  };
 
-    write() {
-        const dir = path.join(__dirname, './templates');
-        const files = fileUtils.readAllFileRecursivelySync(dir)
+  async prompting () {
+    const answers = await this.prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name',
+        default: 'deepexi-eggjs'
+      },
+      {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Your project title',
+        default: 'DeepEXI EggJs'
+      }
+    ])
+    this.props = answers
+  }
 
-        files.forEach(f => {
-            if (fileUtils.isTemplate(f)) {
-                const readmeTpl = _.template(this.fs.read(this.templatePath(f)));
-                this.fs.write(this.destinationPath(fileUtils.tmplToFileName(f)), readmeTpl(this.props));
-            }
-        });
-    }
+  write () {
+    const dir = path.join(__dirname, './templates')
+    const files = fileUtils.readAllFileRecursivelySync(dir)
 
-};
+    files.forEach(f => {
+      if (fileUtils.isTemplate(f)) {
+        const readmeTpl = _.template(this.fs.read(this.templatePath(f)))
+        this.fs.write(this.destinationPath(fileUtils.tmplToFileName(f)), readmeTpl(this.props))
+      }
+    })
+  }
+}
--- generators/test/app/index.test.js
+++ generators/test/app/index.test.js
@@ -1,19 +1,22 @@
-const assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
-const path = require('path');
+'use strict'
+/* eslint-disable no-undef */
+
+const assert = require('yeoman-assert')
+var helpers = require('yeoman-test')
+const path = require('path')
 
 describe('generate app', () => {
-    it('should exists files', () => {
-        return helpers
-            .run(path.join(__dirname, '../../app'))
-            .withPrompts({
-                projectTitle: 'hello',
-                projectName: 'Hello',
-            })
-            .then(function () {
-                assert.file('index.js')
-                assert.file('app/controller/hello.js')
-                assert.file('README.md')
-            });
-    })
-});
+  it('should exists files', () => {
+    return helpers
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        projectTitle: 'hello',
+        projectName: 'Hello'
+      })
+      .then(function () {
+        assert.file('index.js')
+        assert.file('app/controller/hello.js')
+        assert.file('README.md')
+      })
+  })
+})
