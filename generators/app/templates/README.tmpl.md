be7903dc196eb1821f7f7a4595e1d206be515555
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-01 15:23:20 +0800

    new project

---
+++ generators/app/templates/README.tmpl.md
@@ -0,0 +1,3 @@
+# ${ projectTitle }
+
+test
---
+++ generators/test/util/file_utils.test.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const path = require('path');
+const fileUtils = require('../../util/file_utils');
+
+describe('file utils', () => {
+    describe('readAllFileRecursivelySync()', () => {
+        it('should read recursively', () => {
+            const files = fileUtils.readAllFileRecursivelySync(path.join(__dirname, './resources/app'))
+            assert(files.includes('index.js'));
+            assert(files.includes('README.md'));
+            assert(files.includes('controller/home.js'));
+        })
+    })
+
+    describe('isTemplate()', () => {
+        it('is template', () => {
+            assert(fileUtils.isTemplate('foo.tmpl.js'));
+            assert(fileUtils.isTemplate('foo.tmpl'));
+            assert(fileUtils.isTemplate('app/foo.tmpl.js'));
+            assert(fileUtils.isTemplate('app/foo.tmpl'));
+        })
+
+        it('is not template', () => {
+            assert(!fileUtils.isTemplate('foo.js'));
+            assert(!fileUtils.isTemplate('foo'));
+            assert(!fileUtils.isTemplate('footmpl'));
+            assert(!fileUtils.isTemplate('foo.tmpl.'));
+            assert(!fileUtils.isTemplate('foo.tmpl1'));
+        })
+    })
+
+    describe('tmplToFileName()', () => {
+        it('is template', () => {
+            assert.equal(fileUtils.tmplToFileName('foo.tmpl.js'), 'foo.js');
+            assert.equal(fileUtils.tmplToFileName('foo.tmpl'), 'foo');
+            assert.equal(fileUtils.tmplToFileName('app/foo.tmpl.js'), 'app/foo.js');
+        })
+
+        it('is not template', () => {
+            assert.equal(fileUtils.tmplToFileName('foo.js'), 'foo.js');
+            assert.equal(fileUtils.tmplToFileName('foo'), 'foo');
+            assert.equal(fileUtils.tmplToFileName('app/foo.js'), 'app/foo.js');
+        })
+    })
+});
