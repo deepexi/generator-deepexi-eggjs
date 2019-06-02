b4b67036a9859c4d0a8cf929f050da869650dd2d
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-02 16:45:53 +0800

    optimize code

--- generators/app/templates/start-code.tmpl.sh
+++ generators/app/templates/start-code.tmpl.sh
@@ -17,7 +17,7 @@ img_name=${projectName}
 %>
 container_name=${projectName}
 
-docker build -t  $img_name
+docker build -t $img_name
 
 docker rm -f $container_name &> /dev/null
 docker run -d --restart=on-failure:5 \
