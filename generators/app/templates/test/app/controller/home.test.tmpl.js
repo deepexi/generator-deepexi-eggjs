3fa8715461a4f282fbe3b471ddbd64a59863e707
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-02 15:34:48 +0800

    fix unit test error

--- generators/app/templates/test/app/controller/home.test.tmpl.js
+++ generators/app/templates/test/app/controller/home.test.tmpl.js
@@ -12,9 +12,9 @@ describe('test/app/controller/home.test.js', () => {
     // yield ctx.service.xx();
   });
 
-  it('should GET /', () => {
+  it('should GET /${projectName}', () => {
     return app.httpRequest()
-      .get('/')
+      .get('/${projectName}')
       .expect('hi, egg')
       .expect(200)
   });
