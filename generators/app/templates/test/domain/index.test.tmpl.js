bba2fb876779b298420f5f59df9abbcc153dca49
taccisum <liaojinfeng@deepexi.com>
2019-06-11 21:08:11 +0800

    move test folder of domain

--- generators/app/templates/test/domain/index.test.tmpl.js
+++ generators/app/templates/test/domain/index.test.tmpl.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const assert = require('assert');
+ const domain = require('../../modules/domain/index');
 
 describe('modules/domain/test/index.test.js', () => {
     it('should pass', () => {
-        assert(1);
+        assert(domain);
     })
 })
