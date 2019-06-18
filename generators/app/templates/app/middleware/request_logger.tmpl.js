0979f46779df3214b22e2224a52b2360661687e7
taccisum <liaojinfeng@deepexi.com>
2019-06-19 00:15:07 +0800

    optimize request logger middleware and add some comment

--- generators/app/templates/app/middleware/request_logger.tmpl.js
+++ generators/app/templates/app/middleware/request_logger.tmpl.js
@@ -1,11 +1,5 @@
 'use strict';
-const logger = require('koa-logger');
-const _ = require('lodash');
 
-module.exports = (options, app) => {
-  return logger(_.merge({
-    // transporter: (str, args) => {
-    //   app.logger.debug(str);
-    // },
-  }, options));
-};
+// see https://github.com/koajs/logger#koa-logger for more information
+
+module.exports = require('koa-logger');
