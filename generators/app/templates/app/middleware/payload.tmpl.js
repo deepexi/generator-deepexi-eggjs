c97797823504aae78cb12306b14a6c4557aec06c
taccisum <liaojinfeng@deepexi.com>
2019-06-19 00:16:57 +0800

    add comment for payload middleware

--- generators/app/templates/app/middleware/payload.tmpl.js
+++ generators/app/templates/app/middleware/payload.tmpl.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// see https://github.com/deepexi/deepexi-body for more information
+
 module.exports = (opt, app) => {
   return async function payload (ctx, next) {
     await next();
