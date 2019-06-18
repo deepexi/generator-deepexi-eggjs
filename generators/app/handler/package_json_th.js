d377b16a2e034dc5bf3530b61250cd860843a4ce
taccisum <liaojinfeng@deepexi.com>
2019-06-19 00:30:26 +0800

    upgrade deepexi-onerror version to fix bug

--- generators/app/handler/package_json_th.js
+++ generators/app/handler/package_json_th.js
@@ -20,7 +20,7 @@ class PackageJsonTemplateHandler extends AbstractTemplateHandler {
 
   _extend (pkgJson) {
     this._extendDependencies(pkgJson, 'deepexi-body', '^0.0.1');
-    this._extendDependencies(pkgJson, 'deepexi-onerror', '^0.1.0');
+    this._extendDependencies(pkgJson, 'deepexi-onerror', '^0.1.2');
 
     if (this.props.dependencies.utils) {
       this._extendDependencies(pkgJson, '@taccisum/egg-utils', '^0.0.1');
