ff852da28b40e33283d010bafa5e5a9b508fe2a3
taccisum <liaojinfeng@deepexi.com>
2019-06-19 00:30:54 +0800

    fix bug: body undefined

--- generators/app/templates/app/middleware/payload.tmpl.js
+++ generators/app/templates/app/middleware/payload.tmpl.js
@@ -2,8 +2,10 @@
 
 // see https://github.com/deepexi/deepexi-body for more information
 
+const body = require('deepexi-body');
+
 module.exports = (opt, app) => {
-  return async function payload (ctx, next) {
+  return async function payload(ctx, next) {
     await next();
     ctx.body = body.success(ctx.body || '', '操作成功');
   };
