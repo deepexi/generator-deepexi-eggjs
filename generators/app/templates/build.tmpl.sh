1261c55d958917618ae0514e6625b4d28a9bb2da
taccisum <liaojinfeng@deepexi.com>
2019-07-19 15:42:36 +0800

    fix(scaffold): missing char 'v' in image tag

--- generators/app/templates/build.tmpl.sh
+++ generators/app/templates/build.tmpl.sh
@@ -9,14 +9,14 @@ img_name=$\{1:-$\{APP_NAME\}\}
 img_ver=$\{2:-$\{VERSION\}\}
     `)
 %>
-info '准备构建镜像：'$img_name':'$img_ver
+info '准备构建镜像：'$img_name':'v$img_ver
 
 info '获取最新代码'
 git pull
 [ ! $? -eq 0 ] && warn '获取最新代码失败'
 
 info '执行镜像构建'
-docker build -t $img_name':'$img_ver .
+docker build -t $img_name':'v$img_ver .
 if [ $? -eq 0 ];then
     success '完成镜像构建'
 else
