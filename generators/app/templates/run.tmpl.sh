4d04f6562cbb9eb374fd70c023172285e1d0bc87
taccisum <liaojinfeng@deepexi.com>
2019-07-25 16:00:25 +0800

    fix: remove timezone setting

--- generators/app/templates/run.tmpl.sh
+++ generators/app/templates/run.tmpl.sh
@@ -4,7 +4,6 @@ docker run -d --restart=on-failure:5 \
     -p 8080:8080 \
     -v $PWD/logs/:/root/logs/  \
     -v $PWD/run/:/root/run/  \
-    -v /etc/localtime:/etc/localtime \
     -v $PWD/override.js:/root/override.js \
     -e ENV=$ENV \
     -e WORKERS=$WORKERS \
