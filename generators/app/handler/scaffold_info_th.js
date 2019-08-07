304bc20d0e3e88c54e7a29e747d38368b66d8df9
taccisum <liaojinfeng@deepexi.com>
2019-08-07 14:59:50 +0800

    feat(scaffold): provide scaffold_info.md

---
+++ generators/app/handler/scaffold_info_th.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const AbstractTemplateHandler = require('yo-power-generator').AbstractTemplateHandler;
+const _ = require('lodash')
+const fileUtils = require('yo-power-generator').FileUtils;
+const cp = require('child_process');
+const moment = require('moment');
+
+class DefaultTemplateHandler extends AbstractTemplateHandler {
+  _handle0 () {
+    const tpl = _.template(this.generator.fs.read(this.generator.templatePath(this.tmpl)));
+    const destTpl = _.template(fileUtils.tmplToFileName(this.tmpl));
+
+    let yoVer = 'unknown';
+    try {
+      yoVer = cp.execSync('yo --version', { encoding: 'utf-8' }).replace('\r', '').replace('\n', '');
+    } catch (e) {
+      // ingore error for unit test environment
+    }
+
+    const propsCopy = {};
+    _.assignIn(propsCopy, this.props);
+    delete propsCopy.cli;
+
+    this.generator.fs.write(
+      this.generator.destinationPath(destTpl(this.props)),
+      tpl(_.assignIn({
+        toolVersion: 'v' + this.props.version,
+        yoVersion: 'v' + yoVer,
+        nodeVersion: process.version,
+        date: moment().format('YYYY-MM-DD hh:mm:ss'),
+        propsJson: JSON.stringify(propsCopy),
+        generationType: this.props.mode === 'command' ? '命令模式' : '交互模式'
+      }, this.props))
+    )
+  }
+}
+
+module.exports = {
+  key: 'scaffold_info',
+  cls: DefaultTemplateHandler
+};
---
+++ generators/app/templates/scaffold.tmpl_scaffold_info.md
@@ -0,0 +1,15 @@
+# Scaffold参考文档
+
+此项目由[generator-deepexi-eggjs](https://github.com/deepexi/generator-deepexi-eggjs)生成。
+
+如何你有任何问题或优化建议，请到[Github Issues](https://github.com/deepexi/generator-deepexi-eggjs/issues)进行反馈，我们需要你宝贵的意见:-)。
+
+## 项目信息
+
+**工具版本**: ${toolVersion}
+**node版本**: ${nodeVersion}
+**yeoman版本**: ${yoVersion}
+**生成时间**: ${date}
+**生成方式**: ${generationType}
+**生成参数**: ${propsJson}
+**生成命令**: ${cli}
